bf3534d055697793f5985cbcdbad347db4fc07bc
khushwant <khush@KHUSH-DESKTOP>
2022-02-10 14:35:55 -0500

    Adding Comments in Service Provider

--- MNPContactManagementAPI.Tests/ServiceProviderFactory.cs
+++ MNPContactManagementAPI.Tests/ServiceProviderFactory.cs
@@ -15,21 +15,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MNPContactManagementAPI.Tests
 {
+    /// <summary>
+    /// Respective Class hels to setup service provider for XUnit Testing
+    /// </summary>
     public class ServiceProviderFactory
     {
         public ServiceProvider SetupServiceProvider(ITestOutputHelper testOutputHelper)
         {
+            //Set and Get Configuration from appsettings.json
             IConfiguration configuration = UnitTestConfigHelper.GetConfiguration();
 
+
+            //Logging for test output
             ILogger XUnitLogger = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .WriteTo.TestOutput(testOutputHelper, Serilog.Events.LogEventLevel.Verbose)
                 .CreateLogger();
+
+            //create Services
             var services = new ServiceCollection();
 
+            //Add Configuarions into services
             services.AddSingleton(configuration);
             services.AddEntityFrameworkInMemoryDatabase();
-
+            //set up logging
             services.AddLogging(logbuilder => 
             { 
                 logbuilder.AddSerilog(XUnitLogger); 
@@ -37,12 +46,14 @@ namespace MNPContactManagementAPI.Tests
 
             // generating a unique db name for each.
             string dbName = Guid.NewGuid().ToString();
+            //create db context
             services.AddDbContext<MnpContactManagementContext>(options =>
             {
                 options.UseInMemoryDatabase(dbName);
             });
-               
+            //add Interface references
             services.AddTransient<IContactManagementBusinessLogic, ContactManagementBusinessLogic>();
+            //retrun service providers
             return services.BuildServiceProvider();
 
         }
